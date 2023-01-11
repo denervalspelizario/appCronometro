5f2928d6745a05c4e017a02701ade33da9137907
denervalspelizario <denervalspelizario@gmail.com>
2023-01-11 17:34:18 -0300

    update appCronometro criacao de componetes

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'; // 1 - importanto imagem e o touchableopacity que é um tipo de botão
+import Cronometro from './assets/cronometro.png';
+ 
+class App extends Component {
+  render(){
+    return(
+      <View style={styles.container}>  
+        <Image 
+        source={Cronometro}
+        style={styles.image}
+        />
+        <Text style={styles.textCronometro}>0.0</Text>
+        <View style={styles.btnArea}>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnTexto}>Vai</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btn}>
+            <Text style={styles.btnTexto}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00bFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: 200,
+    height: 200,
+  },
+  textCronometro:{
+
+  }
 });
+
+export default App;
